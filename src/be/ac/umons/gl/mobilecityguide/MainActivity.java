f880798c3fbd1f3bdf0d54351182415b46f98665
Kent1 <kent1@github.com>
2012-02-02 23:08:31 +0000

    Ajout de MainActivity
    git-svn-id: http://sgl.umons.ac.be/svn/gl11_5@32 13d25a17-63d0-4e6c-a329-f0202680f6bc

---
+++ src/be/ac/umons/gl/mobilecityguide/MainActivity.java
@@ -0,0 +1,30 @@
+package be.ac.umons.gl.mobilecityguide;
+
+import android.os.Bundle;
+import be.ac.umons.gl.mobilecityguide.R;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+/**
+ * @author Quentin Loos & Hugo Allard
+ *
+ */
+public class MainActivity extends MapActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.main);
+      MapView mapView = (MapView) findViewById(R.id.mapview);
+      mapView.setBuiltInZoomControls(true);
+  }
+
+  /* (non-Javadoc)
+   * @see com.google.android.maps.MapActivity#isRouteDisplayed()
+   */
+  @Override
+  protected boolean isRouteDisplayed() {
+    return false;
+  }
+}
