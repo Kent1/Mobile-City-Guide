0d37fd38b27353c82c82c3e7752dcd1b057ec376
Kent1 <kent1@github.com>
2012-02-03 07:33:39 +0000

    Correction du nom de la classe de test MainActivityTest
    git-svn-id: http://sgl.umons.ac.be/svn/gl11_5@34 13d25a17-63d0-4e6c-a329-f0202680f6bc

---
+++ tests/src/be/ac/umons/gl/mobilecityguide/test/map/MainActivityTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package be.ac.umons.gl.mobilecityguide.test.map;
+
+import be.ac.umons.gl.mobilecityguide.map.MainActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * @author kent
+ *
+ */
+public class MainActivityTest extends
+    ActivityInstrumentationTestCase2<MainActivity> {
+
+  public MainActivityTest() {
+    super("be.ac.umons.gl.mobilecityguide.map", MainActivity.class);
+  }
+
+  /* (non-Javadoc)
+   * @see android.test.ActivityInstrumentationTestCase2#setUp()
+   */
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  /* (non-Javadoc)
+   * @see android.test.ActivityInstrumentationTestCase2#tearDown()
+   */
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  /**
+   * Test method for {@link be.ac.umons.gl.mobilecityguide.map.MainActivity#isRouteDisplayed()}.
+   */
+  public final void testIsRouteDisplayed() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  /**
+   * Test method for {@link be.ac.umons.gl.mobilecityguide.map.MainActivity#onCreate(android.os.Bundle)}.
+   */
+  public final void testOnCreateBundle() {
+    fail("Not yet implemented"); // TODO
+  }
+
+}
