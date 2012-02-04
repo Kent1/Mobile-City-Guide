d56ac5012cce570149b14210d8f9b1a499d5d091
Kent1 <kent1@github.com>
2012-02-04 14:13:28 +0000

    Ajout de POIOverlayItem
    git-svn-id: http://sgl.umons.ac.be/svn/gl11_5@36 13d25a17-63d0-4e6c-a329-f0202680f6bc

---
+++ src/be/ac/umons/gl/mobilecityguide/poi/POIOverlayItem.java
@@ -0,0 +1,67 @@
+package be.ac.umons.gl.mobilecityguide.poi;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.OverlayItem;
+
+/**
+ * OverlayItem of a POI.
+ * 
+ * @author Hugo Allard
+ */
+public class POIOverlayItem extends OverlayItem {
+
+  /** The <code>POI</code> represented by this <code>POIOverlayItem</code>. */
+  private POI poi;
+
+  /**
+   * Constructs a new instance of <code>POIOverlayItem</code>.
+   * 
+   * @param point
+   *          the location of this <code>POIOverlayItem</code>.
+   * @param title
+   *          the title of this <code>POIOverlayItem</code>.
+   * @param snippet
+   *          the snippet of this <code>POIOverlayItem</code>.
+   */
+  public POIOverlayItem(GeoPoint point, String title, String snippet) {
+
+    super(point, title, snippet);
+  }
+
+  /**
+   * Sets the <code>POI</code> represented by this <code>POIOverlayItem</code>.
+   * 
+   * @param the
+   *          <code>POI</code>.
+   */
+  public void setPoi(POI poi) {
+
+    this.poi = poi;
+  }
+
+  /**
+   * Gets the name of this <code>POIOverlayItem</code>.
+   * 
+   * @return the name of the <code>POI</code>
+   */
+  public String getName() {
+
+    return poi.getName();
+  }
+
+  /**
+   * Gets the <code>POI</code> represented by this <code>POIOverlayItem</code>.
+   * 
+   * @return the <code>POI</code>.
+   */
+  public POI getPoi() {
+
+    return poi;
+  }
+
+  @Override
+  public GeoPoint getPoint() {
+
+    return super.getPoint();
+  }
+}
