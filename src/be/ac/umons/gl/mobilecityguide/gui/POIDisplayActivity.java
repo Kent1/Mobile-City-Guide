36c6bf8eca8c3c84f3a8c35d6c1e02fb563848bd
Kent1 <kent1@github.com>
2012-02-05 18:30:40 +0000

    Ajout du prix, durée, et ranking dans POIDisplayActivity
    git-svn-id: http://sgl.umons.ac.be/svn/gl11_5@52 13d25a17-63d0-4e6c-a329-f0202680f6bc

--- src/be/ac/umons/gl/mobilecityguide/gui/POIDisplayActivity.java
+++ src/be/ac/umons/gl/mobilecityguide/gui/POIDisplayActivity.java
@@ -1,44 +1,56 @@
 package be.ac.umons.gl.mobilecityguide.gui;
 
-import be.ac.umons.gl.mobilecityguide.R;
-import be.ac.umons.gl.mobilecityguide.poi.POI;
-import be.ac.umons.gl.mobilecityguide.poi.POIParcelable;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import be.ac.umons.gl.mobilecityguide.R;
+import be.ac.umons.gl.mobilecityguide.db.InfosDB;
+import be.ac.umons.gl.mobilecityguide.db.RankingDB;
+import be.ac.umons.gl.mobilecityguide.poi.POI;
+import be.ac.umons.gl.mobilecityguide.poi.POIParcelable;
 
-public class POIDisplayActivity extends Activity{
-  
+public class POIDisplayActivity extends Activity {
+
   private POI poi;
   private TextView name, address, description, tag, price, duration;
   private RatingBar ratingBar;
-  
+  private final RankingDB rdb = new RankingDB();
+  private final InfosDB idb = new InfosDB();
+
   @Override
-  public void onCreate(Bundle savedInstanceState){
-    
+  public void onCreate(Bundle savedInstanceState) {
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.display_poi);
-    
-    if(getIntent().getParcelableExtra("poi") == null)
+
+    if (getIntent().getParcelableExtra("poi") == null)
       this.finish();
-    
+
     poi = ((POIParcelable) (getIntent().getParcelableExtra("poi"))).getPoi();
-    
+
+    poi.setPrice(idb.getPrice(poi.getId()));
+    poi.setDuration(idb.getDuration(poi.getId()));
+
+    poi.setRank(rdb.getRank(poi.getId()));
+
     name = (TextView) findViewById(R.id.name);
     name.setText(poi.getName());
-    
+
     address = (TextView) findViewById(R.id.address);
     address.setText(poi.getAddress());
-    
+
     description = (TextView) findViewById(R.id.description);
     description.setText(poi.getDescription());
-    
+
     price = (TextView) findViewById(R.id.price);
     price.setText(getString(R.string.price) + " " + poi.getPrice() + "€");
-    
+
     duration = (TextView) findViewById(R.id.duration);
-    duration.setText(getString(R.string.duration) + " " + poi.getDuration() + getString(R.string.minutes));
+    duration.setText(getString(R.string.duration) + " " + poi.getDuration()
+        + getString(R.string.minutes));
+
+    ratingBar = (RatingBar) findViewById(R.id.rating);
+    ratingBar.setRating((float) poi.getRank());
   }
 }
-
