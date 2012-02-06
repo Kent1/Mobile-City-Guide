de3e0eb8df03072bc8b15d32ceab67f2494136b3
Kent1 <kent1@github.com>
2012-02-06 14:29:36 +0000

    Intégration de la DB SQLite dans RankingDB
    git-svn-id: http://sgl.umons.ac.be/svn/gl11_5@63 13d25a17-63d0-4e6c-a329-f0202680f6bc

--- src/be/ac/umons/gl/mobilecityguide/db/RankingDB.java
+++ src/be/ac/umons/gl/mobilecityguide/db/RankingDB.java
@@ -4,6 +4,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 /**
@@ -12,13 +16,35 @@ import android.util.Log;
  * @author Quentin Loos
  */
 public class RankingDB extends DB {
-  /** Tag for log */
-  private static final String tag = "RankingDB";
+  /** DBName & Tag for log */
+  private static final String DBName = "RankingDB";
+
+  /** MyDB */
+  protected MyRankingDB myDB;
+  protected SQLiteDatabase db;
+  private static final String TABLE_RANK = "Ranking";
+  private static final String COL_ID = "Id";
+  private static final String COL_RANK = "Rank";
 
   /**
    * Constructor
    */
-  public RankingDB() {
+  public RankingDB(Context context) {
+    myDB = new MyRankingDB(context, DBName, null, 1);
+  }
+
+  /**
+   * Open the SQLite DB
+   */
+  public void open() {
+    db = myDB.getWritableDatabase();
+  }
+
+  /**
+   * Close the SQLite DB
+   */
+  public void close() {
+    db.close();
   }
 
   /**
@@ -38,24 +64,10 @@ public class RankingDB extends DB {
         json = jsonArray.getJSONObject(0);
         retour = json.getDouble("Rank");
       } catch (JSONException e) {
-        Log.e(tag, "JSONException : " + e.getMessage());
+        Log.e(DBName, "JSONException : " + e.getMessage());
       }
     }
     return retour;
-  }
-
-  /**
-   * Return the rank of a specified POI
-   * 
-   * @param id
-   *          the id of a POI
-   * @return the rank of the POI
-   */
-  public void rank(int id, double rank) {
-    String newRank = "((Rank*NBVote)" + rank + ")/(NBVote+1)";
-    String query = "UPDATE Ranking SET Rank= " + newRank
-        + ", NBVote=NBVote+1 WHERE Id = " + id + "";
-    this.query(query);
   }
 
   /**
@@ -75,9 +87,61 @@ public class RankingDB extends DB {
         json = jsonArray.getJSONObject(0);
         retour = json.getInt("NBVote");
       } catch (JSONException e) {
-        Log.e(tag, "JSONException : " + e.getMessage());
+        Log.e(DBName, "JSONException : " + e.getMessage());
       }
     }
     return retour;
+  }
+
+  /**
+   * Return my rank of a specified POI
+   * 
+   * @param id
+   *          The id of the POI
+   * @return My rank of the POI. 0 if we haven't already vote
+   */
+  public double getMyRank(int id) {
+    open();
+    Cursor cursor = db.query(TABLE_RANK, new String[] { COL_ID, COL_RANK },
+        COL_ID + " = " + id, null, null, null, null);
+    if (cursor.getCount() == 0)
+      return 0.;
+    cursor.moveToFirst();
+    double retour = cursor.getDouble(1);
+    cursor.close();
+    return retour;
+  }
+
+  /**
+   * Rank for a POI
+   * 
+   * @param id
+   *          The POI to rank
+   * @param rank
+   *          The rank that we want to put
+   * @param oldRank
+   *          The oldRank. 0 if we haven't already vote
+   */
+  public void rank(int id, double rank, double oldRank) {
+    String newRank;
+    String newNBVote;
+    if (oldRank == 0) {
+      newRank = "((Rank*NBVote)+" + rank + ")/(NBVote+1)";
+      newNBVote = "NBVote+1";
+    } else {
+      // already vote
+      newRank = "(((Rank*NBVote)-" + oldRank + ")+" + rank + ")/(NBVote)";
+      newNBVote = "NBVote";
+    }
+    String query = "UPDATE Ranking SET Rank= " + newRank + ", NBVote="
+        + newNBVote + " WHERE Id = " + id + "";
+    this.query(query);
+    ContentValues values = new ContentValues();
+    values.put(COL_ID, id);
+    values.put(COL_RANK, rank);
+    if (oldRank == 0)
+      db.insert(TABLE_RANK, null, values);
+    else
+      db.update(TABLE_RANK, values, COL_ID + " = " + id, null);
   }
 }
--- tests/src/be/ac/umons/gl/mobilecityguide/test/db/RankingDBTest.java
+++ tests/src/be/ac/umons/gl/mobilecityguide/test/db/RankingDBTest.java
@@ -1,42 +1,70 @@
 package be.ac.umons.gl.mobilecityguide.test.db;
 
-import be.ac.umons.gl.mobilecityguide.db.RankingDB;
 import android.test.AndroidTestCase;
+import be.ac.umons.gl.mobilecityguide.db.RankingDB;
 
 /**
  * @author Quentin Loos
  */
 public class RankingDBTest extends AndroidTestCase {
   RankingDB db;
-  
-  /* (non-Javadoc)
+
+  /*
+   * (non-Javadoc)
+   * 
    * @see android.test.AndroidTestCase#setUp()
    */
+  @Override
   protected void setUp() throws Exception {
     super.setUp();
-    db = new RankingDB();
+    db = new RankingDB(this.mContext);
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see android.test.AndroidTestCase#tearDown()
    */
+  @Override
   protected void tearDown() throws Exception {
     super.tearDown();
   }
 
   /**
-   * Test method for {@link be.ac.umons.gl.mobilecityguide.db.RankingDB#getRank(int)}.
+   * Test method for
+   * {@link be.ac.umons.gl.mobilecityguide.db.RankingDB#getRank(int)}.
    */
   public final void testGetRank() {
     double rank = db.getRank(1);
-    assertTrue(rank>=0);
+    assertTrue(rank >= 0);
   }
 
   /**
-   * Test method for {@link be.ac.umons.gl.mobilecityguide.db.RankingDB#getNBVote(int)}.
+   * Test method for
+   * {@link be.ac.umons.gl.mobilecityguide.db.RankingDB#getNBVote(int)}.
    */
   public final void testGetNBVote() {
     int nb = db.getNBVote(1);
-    assertTrue(nb>=0);
+    assertTrue(nb >= 0);
+  }
+
+  /**
+   * Test method for
+   * {@link be.ac.umons.gl.mobilecityguide.db.RankingDB#getMyRank(int)}.
+   */
+  public final void testGetMyRank() {
+    double nb = db.getMyRank(1);
+    assertTrue(nb >= 0);
+    assertTrue(nb <= 5);
+  }
+
+  /**
+   * Test method for
+   * {@link be.ac.umons.gl.mobilecityguide.db.RankingDB#rank(int, double, double)}
+   */
+  public final void testRank() {
+    db.rank(1, 1, 0);
+    db.rank(1, 5, 1);
+    assertTrue(true);
   }
 }
