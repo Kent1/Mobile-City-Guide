6af9d0ee49a9b312cb9731e7562981de36a20be0
Kent1 <kent1@github.com>
2012-02-10 13:57:46 +0000

    Ajout de Iterator dans Itinerary
    git-svn-id: http://sgl.umons.ac.be/svn/gl11_5@98 13d25a17-63d0-4e6c-a329-f0202680f6bc

--- src/be/ac/umons/gl/mobilecityguide/poi/Itinerary.java
+++ src/be/ac/umons/gl/mobilecityguide/poi/Itinerary.java
@@ -1,95 +1,121 @@
 package be.ac.umons.gl.mobilecityguide.poi;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * An <code>Itinerary</code> is a <code>List</code> of <code>POI</code>s.
  */
-public class Itinerary{
+public class Itinerary implements Iterator<POI> {
 
-  /** Contains all the <code>POI</code>s for this <code>Itinerary</code>.*/
+  /** Contains all the <code>POI</code>s for this <code>Itinerary</code>. */
   private List<POI> list;
-  
+  /** Itinerary's Iterator */
+  private final Iterator<POI> iterator;
+
   /**
    * Constructs a new empty instance of <code>Itinerary</code>.
    */
-  public Itinerary(){
-    
+  public Itinerary() {
+
     list = new ArrayList<POI>();
+    iterator = list.iterator();
   }
-  
-  public void optimize(){
-    
-    //TODO TSP
+
+  public void optimize() {
+
+    // TODO TSP
   }
 
   /**
    * Adds the specified <code>POI</code> to this <code>Itinerary</code>.
    * 
-   * @param poi the <code>POI</code> to add.
+   * @param poi
+   *          the <code>POI</code> to add.
    */
-  public void add(POI poi){
-    
+  public void add(POI poi) {
+
     list.add(poi);
   }
-  
+
   /**
    * Empties this <code>Itinerary</code>.
    */
-  public void clear(){
-    
+  public void clear() {
+
     list.clear();
   }
-  
+
   /**
    * Searches this <code>Itinerary</code> for the specified <code>POI</code>.
    * 
-   * @param poi the <code>POI</code> to search for.
-   * @return <code>true</code> if the <code>POI</code> is present, <code>false</code> otherwise.
+   * @param poi
+   *          the <code>POI</code> to search for.
+   * @return <code>true</code> if the <code>POI</code> is present,
+   *         <code>false</code> otherwise.
    */
-  public boolean contains(POI poi){
-    
+  public boolean contains(POI poi) {
+
     return list.contains(poi);
   }
-  
+
   /**
    * Removes the specified <code>POI</code> from this <code>Itinerary</code>.
    * 
-   * @param poi the <code>POI</code> to remove.
+   * @param poi
+   *          the <code>POI</code> to remove.
    */
-  public void remove(POI poi){
-    
+  public void remove(POI poi) {
+
     list.remove(poi);
   }
-  
+
   /**
    * Returns the number of <code>POI</code>s in this <code>Itinerary</code>.
    * 
    * @return the number of <code>POI</code>s in this <code>Itinerary</code>.
    */
-  public int size(){
-    
+  public int size() {
+
     return list.size();
   }
-  
+
   /**
    * Replaces the <code>List</code> of <code>POI</code>s.
-   *  
-   * @param list a <code>List</code> representing an <code>Itinerary</code>.
+   * 
+   * @param list
+   *          a <code>List</code> representing an <code>Itinerary</code>.
    */
-  public void setList(List<POI> list){
-    
+  public void setList(List<POI> list) {
+
     this.list = list;
   }
-  
+
   /**
-   * Returns the <code>List</code> with all the <code>POI</code>s in this <code>Itinerary</code>.
+   * Returns the <code>List</code> with all the <code>POI</code>s in this
+   * <code>Itinerary</code>.
    * 
-   * @return a <code>Set</code> with all the <code>POI</code>s in this <code>Itinerary</code>.
+   * @return a <code>Set</code> with all the <code>POI</code>s in this
+   *         <code>Itinerary</code>.
    */
-  public List<POI> getList(){
-    
+  public List<POI> getList() {
+
     return list;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return iterator.hasNext();
+  }
+
+  @Override
+  public POI next() {
+    return iterator.next();
+  }
+
+  @Override
+  public void remove() {
+    iterator.remove();
   }
 }
